c4221cc27450f4612683077b872b35c8aed02df4
AraceliDC <agdc.105@gmail.com>
2024-05-20 20:19:48 -0400

    movimiento carpetas

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,4 +4,9 @@ const axiosClient = axios.create({
     baseURL: process.env.BACKEND_URL
 })
 
+axios.get(process.env.BACKEND_URL)
+  .catch(function (error) {
+    console.log(error);
+  });
+
 export default axiosClient
